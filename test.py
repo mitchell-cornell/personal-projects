9f052ae5d3c4d93f8b2be90cc637dc7fbf8a00d1
Mitchell <mitcorn@sas.upenn.edu>
2020-03-18 12:12:23 -0700

    Test to see if commiting changes works

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 for y in range(5):
     print(y*6)
 
+c = True
+
+while c:
+    print('what it do homie')
+    c = False
+
 """
-This is a test file
+This is a test file. 
+
+Added lines to ensure ability to commit changes
 """
+
